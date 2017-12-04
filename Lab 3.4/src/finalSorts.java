9cf99a385cc291adb18bd73c9a558fdf8fb18a47
GG_Family <GG_Family@ivy-PC>
2017-12-03 22:16:01 -0500

    12/3/17

---
+++ Lab 3.4/src/finalSorts.java
@@ -0,0 +1,17 @@
+
+public class finalSorts {
+	public static void main(String[] args)
+	{
+		
+	}
+	public static int partition(int [] list1, int front, int back)
+	{
+		int pivot = list1[front];
+		return pivot;
+	}
+	
+	public static void quickSort(int[] list1, int front, int back)
+	{
+		
+	}
+}
