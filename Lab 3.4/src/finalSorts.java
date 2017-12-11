891a83b85dd1a0ae30fb50b361e04225715df871
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-12-11 12:54:03 -0500

    still need to code own sorting method

--- Lab 3.4/src/finalSorts.java
+++ Lab 3.4/src/finalSorts.java
@@ -2,8 +2,17 @@
 public class finalSorts {
 	public static void main(String[] args)
 	{
-		int[] testList = {4, 5, 2, 6, -5};
+		// partition test case
+		/*int[] testList = {-10, 700, 4, 5, 2, 6, -5};
 		System.out.println(partition(testList, 0, testList.length));
+		for(int i = 0; i < testList.length; i++)
+		{
+			System.out.println(testList[i]);
+		}*/
+		
+		//quickSort test case
+		int[] testList = {-10, 700, 4, 5, 2, 6, -5};
+		quickSort(testList, 0, testList.length);
 		for(int i = 0; i < testList.length; i++)
 		{
 			System.out.println(testList[i]);
@@ -11,26 +20,29 @@ public class finalSorts {
 	}
 	public static int partition(int [] list1, int front, int back)
 	{
-		int pivot = front;
-		for(int i = front; i < back; i++)
+		int pivot = (back + front)/2;
+		boolean sorted = false;
+		while(!sorted)
 		{
-			int swap;
-			int swap2;
-			if(list1[pivot] >= list1[i])
+			sorted = true;
+			for(int i = front; i < back; i++)
 			{
-				swap = list1[pivot];
-				list1[pivot] = list1[i];
-				list1[i] = swap;
-				pivot = i;
-			}
-			for(int j = back; j < front; j--)
-			{
-				if(list1[pivot] <= list1[j])
+				int swap;
+				if(list1[pivot] <= list1[i] && pivot > i)
 				{
-					swap2 = list1[pivot];
-					list1[pivot] = list1[j];
-					list1[j] = swap2;
-					pivot = j;
+					swap = list1[pivot];
+					list1[pivot] = list1[i];
+					list1[i] = swap;
+					pivot = i;
+					sorted = false;
+				}
+				if(list1[pivot] >= list1[i] && pivot < i)
+				{
+					swap = list1[pivot];
+					list1[pivot] = list1[i];
+					list1[i] = swap;
+					pivot = i;
+					sorted = false;
 				}
 			}
 		}
@@ -38,6 +50,16 @@ public class finalSorts {
 	}
 	
 	public static void quickSort(int[] list1, int front, int back)
+	{
+		if(front != back)
+		{
+			int piv = partition(list1, front, back);
+			partition(list1, front, piv);
+			partition(list1, piv, back);
+		}
+	}
+	
+	public static void shellSort(int[] list1)
 	{
 		
 	}
